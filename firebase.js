eac8cc510f0d35bccf1678e6c5c1da36d840615b
ResellerGaming <resellergaming.official@gmail.com>
2025-07-20 11:34:30 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,10 +1,10 @@
 // Ganti dengan config project Firebase kamu
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAhGpWcCsMQF7s9Iz6YiZCoRh1iUlcz-9g",
+  authDomain: "database-by-reseller-gaming.firebaseapp.com",
+  projectId: "database-by-reseller-gaming",
+  storageBucket: "database-by-reseller-gaming.firebasestorage.app",
+  messagingSenderId: "846227590729",
+  appId: "1:846227590729:web:8874267516c7aca0813998"
 };
 firebase.initializeApp(firebaseConfig);
