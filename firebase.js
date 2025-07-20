e0d0c85fc86057ca761c7ce089aeb667babc3a07
ResellerGaming <resellergaming.official@gmail.com>
2025-07-20 11:42:16 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,7 +3,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAhGpWcCsMQF7s9Iz6YiZCoRh1iUlcz-9g",
   authDomain: "database-by-reseller-gaming.firebaseapp.com",
   projectId: "database-by-reseller-gaming",
-  storageBucket: "database-by-reseller-gaming.firebasestorage.app",
+  storageBucket: "database-by-reseller-gaming.firebasestorage.com",
   messagingSenderId: "846227590729",
   appId: "1:846227590729:web:8874267516c7aca0813998"
 };
